16560a259b8e081ff247e35475b8967657297be8
jocode <a15985869670@hotmail.com>
2018-05-24 22:20:47 +0800

    创建
    拥有彩虹颜色的进度条

---
+++ js/tokyoHot.js
@@ -0,0 +1,26 @@
+﻿var setPeogress = function(ele, start, stop, time) {
+    var timeProress = 0
+    var progressBar_Box = document.getElementById(ele)
+    var progress = progressBar_Box.firstElementChild
+    var title = progressBar_Box.lastElementChild
+    progress.style.width = start + "%"
+    var temp = (stop - start) / (time / 100)
+    var temp1 = start
+    var myVar = setInterval(function() {
+        temp1 += temp
+        progress.style.width = temp1 + "%"
+        timeProress += 1 / 10
+        title.innerHTML = parseInt(temp1) + "%"
+        console.log(timeProress)
+    }, 100)
+    setTimeout(function() {
+        clearInterval(myVar)
+    }, time);
+}
+
+var start = function() {
+    console.log('start')
+    setPeogress('progressBar_Box', 0, 100, 10000)
+}
+var start_btn = document.getElementById('peogress_start')
+start_btn.onclick = start
