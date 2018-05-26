3adfbb0cdbe51c4019ed83bbf6e5be41234b82bc
jocode <a15985869670@hotmail.com>
2018-05-26 19:11:20 +0800

    添加拖动按钮，跟随进度条移动，暂不可拖动

--- js/tokyoHot.js
+++ js/tokyoHot.js
@@ -3,12 +3,14 @@
     var progressBar_Box = document.getElementById(ele)
     var progress = progressBar_Box.firstElementChild
     var title = progressBar_Box.lastElementChild
+    var dragDutton = document.getElementById('td_btn')
     progress.style.width = start + "%"
     var temp = (stop - start) / (time / 100)
     var temp1 = start
     var myVar = setInterval(function() {
         temp1 += temp
         progress.style.width = temp1 + "%"
+        dragDutton.style.marginLeft = temp1 - 1 + "%"
         timeProress += 1 / 10
         title.innerHTML = parseInt(temp1) + "%"
         console.log(timeProress)
